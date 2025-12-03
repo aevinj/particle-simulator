990e9b5c8846316710143a8a99f6382b333db5b0
aevinj <ajjaevinjais@gmail.com>
2025-12-03 23:13:25 +0000

    fix: change from pair<float, float> to sf::Vector2f

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ const float GRAVITY = 800.f;
 const float DAMPENING = 0.8f;
 
 struct Particle {
-        std::pair<float, float> velocity;
+        sf::Vector2f velocity;
         sf::CircleShape particle;
 
-        Particle(std::pair<float, float> velocity) : velocity(velocity) , particle(5.f) {
+        Particle(sf::Vector2f velocity) : velocity(velocity) , particle(5.f) {
                 particle.setFillColor(sf::Color::Green);
                 particle.setOrigin(particle.getRadius(), particle.getRadius());
                 particle.setPosition(static_cast<float>(SCREEN_WIDTH / 2), static_cast<float>(SCREEN_HEIGHT / 2));
@@ -18,19 +18,19 @@ struct Particle {
         void updatePos(float dt) {
                 auto [x,y] = particle.getPosition();
         updateVelocities(dt);
-                x += velocity.first * dt;
-                y += velocity.second * dt;
+                x += velocity.x * dt;
+                y += velocity.y * dt;
 
         if (y + particle.getRadius() >= SCREEN_HEIGHT) {
             y = SCREEN_HEIGHT - particle.getRadius();
-            velocity.second = -velocity.second * DAMPENING;
+            velocity.y = -velocity.y * DAMPENING;
         }
 
                 particle.setPosition(x, y);
         }
 
     void updateVelocities(float dt) {
-        velocity.second += GRAVITY * dt;
+        velocity.y += GRAVITY * dt;
     }
 
         sf::CircleShape& getParticle() {return particle;}
@@ -40,7 +40,7 @@ int main() {
         sf::RenderWindow window(sf::VideoMode(800,600), "Particle test");
         window.setFramerateLimit(120);
 
-    sf::Clock clock;
+        sf::Clock clock;
 
         Particle particle({0.f,0.f});
 
@@ -50,12 +50,13 @@ int main() {
                 while (window.pollEvent(event)) {
                         if (event.type == sf::Event::Closed) window.close();
                 }
-        dt = clock.restart().asSeconds();
+                dt = clock.restart().asSeconds();
+
+                particle.updatePos(dt);
+
                 window.clear(sf::Color::Black);
                 window.draw(particle.getParticle());
                 window.display();
-
-                particle.updatePos(dt);
         }
         return 0;
 }
