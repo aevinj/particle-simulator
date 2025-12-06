21839933b6b166e49e8a2511bc4fb55a8411be18
aevinj <ajjaevinjais@gmail.com>
2025-12-06 22:07:56 +0000

    feat: dynamic multiple particle spawning

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@
 
 const int SCREEN_HEIGHT = 600;
 const int SCREEN_WIDTH = 800;
+const float SPAWN_DELAY = 0.005f;
+const int PARTICLE_COUNT = 1000;
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Particle Simulator");
     window.setFramerateLimit(60);
 
-    sf::Clock clock;
+    sf::Clock clock, spawner;
     float dt;
     std::vector<Particle> particles;
-    particles.emplace_back(sf::Vector2f(300.f,300.f), 9.f);
+    const sf::Vector2f starting_vel(1000.f, 1000.f);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -22,6 +24,12 @@ int main() {
             }
         }
         dt = clock.restart().asSeconds();
+
+        if (particles.size() < PARTICLE_COUNT && spawner.getElapsedTime().asSeconds() >= SPAWN_DELAY) {
+            particles.emplace_back(sf::Vector2f(100.f,100.f), 5.f);
+            auto& latest = particles.back();
+            latest.prev_position = latest.position - starting_vel * dt;
+        }
 
         window.clear(sf::Color::White);
 
