9296dcc6be1a7b80aec1ebf5f2977829bb0ffd6b
aevinj <ajjaevinjais@gmail.com>
2025-12-11 15:16:25 +0000

    refactor: move visual text class to World.hpp

--- World.hpp
+++ World.hpp
@@ -183,3 +183,38 @@ class World {
             renderer.draw(window);
         }
 };
+
+class VisualText {
+    private:
+        sf::Font font;
+        sf::Text particleCount, timeBetweenFrames;
+
+    public:
+        VisualText() {
+            font.loadFromFile("../assets/arial.ttf");
+
+            particleCount.setFont(font);
+            particleCount.setString("--");
+            particleCount.setCharacterSize(24);
+            particleCount.setFillColor(sf::Color::Black);
+
+            timeBetweenFrames.setFont(font);
+            timeBetweenFrames.setString("--");
+            timeBetweenFrames.setCharacterSize(24);
+            timeBetweenFrames.setFillColor(sf::Color::Black);
+            timeBetweenFrames.setPosition(0.f, 30.f);
+        }
+
+        void draw(sf::RenderWindow &window) const {
+            window.draw(particleCount);
+            window.draw(timeBetweenFrames);
+        }
+
+        void setParticle(std::string cnt) {
+            particleCount.setString(cnt);
+        }
+
+        void setFrames(std::string cnt) {
+            timeBetweenFrames.setString(cnt + "ms");
+        }
+};
